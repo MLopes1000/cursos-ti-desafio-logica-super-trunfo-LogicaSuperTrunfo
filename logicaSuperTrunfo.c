0016a68b1e7b3adfc46b3a012b079f5c97385136
MLopes1000 <marcelo.olavo@hotmail.com>
2026-04-17 18:50:12 +0000

    Desafio nível novato concluído

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,10 +6,19 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
-    
+    char estado;
+    char codigoDaCarta [50];
+    char nomeDaCidade [50];
+    float populacao;
+    int numeroDePontosTuristicos;
+    float area, pib;
+    float densidadePopulacional;
+    float pibPerCapita;
+
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
@@ -20,6 +29,71 @@ int main() {
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
+
+    printf("Carta 1\n");
+
+    printf("Estado: ");
+    scanf("%s", &estado);
+
+    printf("código: ");
+    scanf("%s", &codigoDaCarta);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", nomeDaCidade);
+
+    printf("População: ");
+    scanf("%f", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+    densidadePopulacional = populacao  /area;
+
+     printf("Pib: ");
+    scanf("%f", &pib);
+
+    pibPerCapita = (pib * 1000000) / populacao;
+
+    printf("Número de pontos turísticos: ");
+    scanf("%f", &numeroDePontosTuristicos);
+
+    printf("Densidade populacional: %.2f\n", densidadePopulacional);
+    
+    printf("PIB per capita: %f\n", pibPerCapita);
+
+    printf("\n");
+
+    
+     printf("Carta 2\n");
+
+    printf("Estado: ");
+    scanf("%s", &estado);
+
+    printf("código: ");
+    scanf("%s", &codigoDaCarta);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", nomeDaCidade);
+
+    printf("População: ");
+    scanf("%f", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+    densidadePopulacional = populacao  /area;
+
+     printf("Pib: ");
+    scanf("%f", &pib);
+
+    pibPerCapita = (pib * 1000000) / populacao;
+
+     printf("Número de pontos turísticos: ");
+    scanf("%f", &numeroDePontosTuristicos);
+
+    printf("Densidade populacional: %.2f\n", densidadePopulacional);
+    
+    printf("PIB per capita: %f\n", pibPerCapita);
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
